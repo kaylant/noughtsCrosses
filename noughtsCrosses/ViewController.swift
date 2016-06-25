18770c181f8068d77b04f9a5eb1b12235da45040
kaylant <kaylant@gmail.com>
2016-06-25 16:25:10 -0500

    players set up

--- noughtsCrosses/ViewController.swift
+++ noughtsCrosses/ViewController.swift
@@ -9,13 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activePlayer = 1 // 1 = noughts, 2 = crosses
+    
+    var gameState = [0,0,0,0,0,0,0,0,0]
 
     @IBOutlet var button: UIButton!
     
     // modify sender to know which button is tapped
     @IBAction func buttonPressed(sender: AnyObject) {
         
-        sender.setImage(UIImage(named: "cross.png"), forState: .Normal)
+        if (gameState[sender.tag] == 0) {
+            
+            gameState[sender.tag] = activePlayer
+        
+            if activePlayer == 1 {
+            
+                sender.setImage(UIImage(named: "nought.png"), forState: .Normal)
+                
+                activePlayer = 2
+                
+                
+            } else {
+                
+                sender.setImage(UIImage(named: "cross.png"), forState: .Normal)
+                
+                activePlayer = 1
+                
+            }
+            
+        }
         
     }
     
